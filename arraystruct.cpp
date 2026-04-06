048207035b441742d716847ba086e7179c0c2b7c
hudangrh <hudangrhkuliah@gmail.com>
2026-04-06 16:23:00 +0700

    arraystructnew3

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -16,3 +16,14 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs[3];
+
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "Nama : " ;
+        getline(cin, mhs[i].nama);
+        cout << "Alamat Desa: " ;
+        getline(cin, mhs[i].alamat.desa);
+        cout << "Alamat Kota: " ;
